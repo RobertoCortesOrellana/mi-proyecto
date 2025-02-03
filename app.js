fce3cf35afcf4ad7900793cd2aa18ea90d06043e
RobertoCortesOrellana <rocorteso@yahoo.com>
2025-02-02 22:37:29 -0300

    Etapa realizada implementa función agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Declaracion de Variables
+let arreglo = []
+
+// Función que obtiene el nombre de la caja de texto, si no es vacío lo incorpora al arreglo y luego borra la caja de texto
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+    if (nombreAmigo == ""){
+        alert("Por favor, inserte un nombre.");
+    } else {
+        arreglo.push(nombreAmigo);
+        console.log(arreglo);
+        LimpiaCaja()
+    }
+return;
+}
+
+// Función que limpia la caja de texo
+function LimpiaCaja(){
+    document.getElementById('amigo').value = "";
+return
+}
